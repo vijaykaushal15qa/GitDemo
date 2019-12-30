959571e269510b557a34685e2c7f0ff2954af611
Vijay Kaushal <vijaykaushal15qa@gmail.com>
2019-12-30 18:45:39 +0530

    Again commit by first person

--- src/test/java/TestFinalFramework/HomePage.java
+++ src/test/java/TestFinalFramework/HomePage.java
@@ -49,6 +49,8 @@ public void homePageNavigation(String Username, String Password, String text) th
 	 lp1.clicLogin().click();
 	 System.out.println("GIT DEMO1");
 	 System.out.println("GIT DEMO2");
+         System.out.println("GIT DEMO3");
+	 System.out.println("GIT DEMO4");
 
 	 
 }
